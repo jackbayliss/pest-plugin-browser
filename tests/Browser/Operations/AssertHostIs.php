1c6cb8ed97664b0084606676d4580b1b5aa47991
Jack <jjbayliss@icloud.com>
2025-02-18 00:16:44 +0000

    test peck

--- tests/Browser/Operations/AssertHostIs.php
+++ tests/Browser/Operations/AssertHostIs.php
@@ -6,20 +6,20 @@ test('assert host is', function () {
     $this->visit('http://localhost:9357')
         ->assertHostIs('localhost:9357');
 });
-//autoload
-//bindable
-//cachable
-//chunkable
-//configurable
-//deferred
-//dereference
-//deserializable
-//downloadable
-//extensible
-//fluent
-//immutable
-//invisible
-//iterable
-//reusable
-//serializable
-//subscribable
+// autoload
+// bindable
+// cachable
+// chunkable
+// configurable
+// deferred
+// dereference
+// deserializable
+// downloadable
+// extensible
+// fluent
+// immutable
+// invisible
+// iterable
+// reusable
+// serializable
+// subscribable
